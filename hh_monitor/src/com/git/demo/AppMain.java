2a9e6da8d5e56673ac21aabe6ab65f191655cbf6
yangquanhui <yangquanhui@gd.zc-insurance.com>
2018-12-26 16:58:41 +0800

    ssss

--- hh_monitor/src/com/git/demo/AppMain.java
+++ hh_monitor/src/com/git/demo/AppMain.java
@@ -2,6 +2,6 @@ package com.git.demo;
 
 public class AppMain {
     public static void main(String args[]) {
-    	System.out.println("≤‚ ‘");
+    	System.out.println("AppMain≤‚ ‘");
     }
 }
--- hh_monitor/src/com/git/demo/HuochePiao.java
+++ hh_monitor/src/com/git/demo/HuochePiao.java
@@ -1,5 +1,7 @@
 package com.git.demo;
 
 public class HuochePiao {
-
+	public static void main(String args[]) {
+    	System.out.println("HuochePiao≤‚ ‘");
+    }
 }
